6d7d6cf000bc5b9d2d2bd558a573ccf7477d69f6
Sam Ingram <31179948+SamPIngram@users.noreply.github.com>
2023-11-20 20:08:25 +0000

    testing faster compute options

---
+++ benchmark.py
@@ -0,0 +1,87 @@
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+import math
+
+def randomGrid(N):
+    """ returns a grid of NxN random values"""
+    return np.random.choice(vals, N*N, p=[0.2, 0.8]).reshape(N,N)
+
+def update_grid_orig(img, grid, N):
+
+    newGrid = grid.copy()
+    for i in range(N):
+        for j in range(N):
+
+            # compute 8-neighbor sum
+            # using toroidal boundary conditions - x and y wrap around
+            # so that the simulation takes place on a toroidal surface
+            total = int((grid[i, (j-1)%N] + grid[i, (j+1)%N] + 
+                        grid[(i-1)%N, j] + grid[(i+1)%N, j] + 
+                        grid[(i-1)%N, (j-1)%N] + grid[(i-1)%N, (j+1)%N] +
+                        grid[(i+1)%N, (j-1)%N] + grid[(i+1)%N, (j+1)%N])/255)
+            
+            # apply Conway's rules
+            if grid[i, j] == ON:
+                if (total < 2) or (total > 3):
+                    newGrid[i, j] = OFF
+            else:
+                if total == 3:
+                    newGrid[i, j] = ON
+    
+    # update data
+    img.set_data(newGrid)
+    grid[:] = newGrid[:]
+    return img
+
+def get_grid_npvec(grid, idx, N):
+    grid = grid.reshape(N,N)
+    i = math.floor(idx / N)
+    j = idx % N
+    print(grid)
+    total = int((grid[i, (j-1)%N] + grid[i, (j+1)%N] + 
+                grid[(i-1)%N, j] + grid[(i+1)%N, j] + 
+                grid[(i-1)%N, (j-1)%N] + grid[(i-1)%N, (j+1)%N] +
+                grid[(i+1)%N, (j-1)%N] + grid[(i+1)%N, (j+1)%N])/255)
+    # apply Conway's rules
+    if grid[i, j] == ON:
+        if (total < 2) or (total > 3):
+            return OFF
+    else:
+        if total == 3:
+            return ON
+
+def update_grid_npvec(img, grid, N):
+    newGrid = grid.copy()
+    vfunc = np.vectorize(get_grid_npvec)
+    newGrid = newGrid.flatten()
+    vfunc(grid.flatten(), list(range(len(newGrid))), N)
+    newGrid = newGrid.reshape(N,N)
+    # update data
+    img.set_data(newGrid)
+    grid[:] = newGrid[:]
+    return img
+
+if __name__ == '__main__':
+    runs = 100
+    N = 100
+
+    ON = 255
+    OFF = 0
+    vals = [ON, OFF]
+
+    grid = randomGrid(N)
+    fig, ax = plt.subplots()
+    img = ax.imshow(grid, interpolation='nearest')
+
+    time_start = time.perf_counter()
+    for i in range(runs):
+        update_grid_orig(img, grid, N)
+    time_end = time.perf_counter()
+    print(f"Original: {time_end - time_start:0.4f} seconds")
+
+    time_start = time.perf_counter()
+    for i in range(runs):
+        update_grid_npvec(img, grid, N)
+    time_end = time.perf_counter()
+    print(f"NumPy Vectorised: {time_end - time_start:0.4f} seconds")
